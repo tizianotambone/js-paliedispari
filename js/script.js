8850d2ebec3ab786016c4cf7af67b1445c22eee3
tizianotambone <tambonetiziano@gmail.com>
2025-03-21 18:16:09 +0100

    [UPD] ciclo for 1 ex

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
 
 let parolainvertita = prompt("inserisci una parola");
 
 function palindroma(words){
-    let parolaInvertita = words.split("").reverse().join("");
+    // let parolaInvertita = words.split("").reverse().join("");
 
+    // inizializzo una variabile
+    let parolaInvertita = "";
+
+    // ciclo partendo dall'ultima posizione della parola che viene dall'argomento della funzione (words)
+    for (let i = words.length -1;  i>0; i--) {
+
+        parolaInvertita = words[i]
+        console.log('nel ciclo', parolaInvertita)
+    }
+
+    console.log('fuori dal ciclo', parolaInvertita)
     return words === parolaInvertita;
 }
 
-
-
+// se la funzione mi retituisce true o false appare lo script parola palindroma altrimenti appare parola non palindroma
+if(palindroma(parolainvertita)){
+    console.log("parola palindroma")
+}
+else{
+    console.log("parola non palindroma")
+}
 
         
         
